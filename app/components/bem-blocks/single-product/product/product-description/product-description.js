4518ebf517a9d037110d5efd6e3348bf73b2d69d
Sergey Stoyko <sergeysinoptik@gmail.com>
2022-08-22 09:23:06 +0300

    popup product imgs & fix links

---
+++ app/components/bem-blocks/single-product/product/product-description/product-description.js
@@ -0,0 +1,18 @@
+$('.product-description').magnificPopup({
+  delegate: 'a',
+  type: 'image',
+  gallery:{
+    enabled:true
+  },
+  removalDelay: 300,
+  zoom: {
+    enabled: true, // By default it's false, so don't forget to enable it
+
+    duration: 300, // duration of the effect, in milliseconds
+    easing: 'ease-in-out', // CSS transition easing function
+
+    
+  }
+
+  // other options
+});
