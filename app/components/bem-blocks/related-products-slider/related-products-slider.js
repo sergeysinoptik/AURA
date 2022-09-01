8dfaa0a31060cf27be24a88e16a5b498fe50a9af
Sergey Stoyko <sergeysinoptik@gmail.com>
2022-09-01 08:33:33 +0300

    related products slider fix

---
+++ app/components/bem-blocks/related-products-slider/related-products-slider.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+const $carouselle = $('.products-slider');
+
+const productsSliderSettings = {
+  dots: false,
+  arrows: true,
+  slide: '.slick-slideshow__slide',
+  slidesToShow: 4,
+  infinite: false,
+  // centerMode: false,
+  // centerPadding: '60px',
+};
+
+function setSlideVisibility() {
+  // Find the visible slides i.e. where aria-hidden="false"
+  const visibleSlides = $carouselle.find('.slick-slideshow__slide[aria-hidden="false"]');
+  // Make sure all of the visible slides have an opacity of 1
+  $(visibleSlides).each(function() {
+    $(this).css('opacity', 1);
+  });
+
+  // Set the opacity of the first and last partial slides.
+  $(visibleSlides).first().prev().css('opacity', 0);
+}
+
+$carouselle.slick(productsSliderSettings);
+$carouselle.slick('slickGoTo', 1);
+setSlideVisibility();
+
+$carouselle.on('afterChange', () => {
+  setSlideVisibility();
+});
