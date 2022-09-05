3e99600f0a98eea7ec49a2f4e35f9f53bcf76a56
Sergey Stoyko <sergeysinoptik@gmail.com>
2022-09-05 08:50:09 +0300

    mobile menu contacts button UI

--- app/components/bem-blocks/icon-contacts/icon-contacts.js
+++ app/components/bem-blocks/icon-contacts/icon-contacts.js
@@ -2,7 +2,7 @@
 const iconContacts = document.querySelector('#icon-contacts');
 
 function contactsToggle() {
-  const contactsDropdown = select('.contacts-dropdown');
+  const contactsDropdown = select('#icon-contacts .contacts-dropdown');
 
   iconContacts.addEventListener('click', () => {
     contactsDropdown.classList.toggle('contacts-dropdown_active');
@@ -19,4 +19,25 @@ function contactsToggle() {
   });
 }
 
+const iconContactsMob = document.querySelector('#contacts-mob');
+
+function contactsToggleMob() {
+  const contactsDropdownMob = document.querySelector('#contacts-mob .contacts-dropdown');
+
+  iconContactsMob.addEventListener('click', () => {
+    contactsDropdownMob.classList.toggle('contacts-dropdown_active');
+  });
+
+  document.addEventListener('click', (e) => {
+    const { target } = e;
+    const itsDropdown = target === iconContactsMob || iconContactsMob.contains(target);
+    const isActive = contactsDropdownMob.classList.contains('contacts-dropdown_active');
+
+    if (!itsDropdown && isActive) {
+      contactsDropdownMob.classList.toggle('contacts-dropdown_active');
+    }
+  });
+}
+
 window.onload = contactsToggle();
+window.onload = contactsToggleMob();
