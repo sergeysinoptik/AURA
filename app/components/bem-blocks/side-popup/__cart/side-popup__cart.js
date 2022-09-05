3b424fc3a6eab4ce282296afd2bd24311d9e8a89
Sergey Stoyko <sergeysinoptik@gmail.com>
2022-09-05 10:12:30 +0300

    mobile menu side cart UI

--- app/components/bem-blocks/side-popup/__cart/side-popup__cart.js
+++ app/components/bem-blocks/side-popup/__cart/side-popup__cart.js
@@ -11,3 +11,16 @@ jQuery(document).ready(($) => {
     }
   });
 });
+
+jQuery(document).ready(($) => {
+  $('#cart-mob').on('click', (event) => {
+    event.preventDefault();
+    $('#side-popup-cart').addClass('is-visible');
+  });
+  $('#side-popup-cart').on('click', function(event) {
+    if ($(event.target).is('.side-popup_close') || $(event.target).is('#side-popup-cart')) {
+      event.preventDefault();
+      $(this).removeClass('is-visible');
+    }
+  });
+});
