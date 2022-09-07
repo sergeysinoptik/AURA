784d887ee042fe61c4004f5759d1f2ae71d65c05
Sergey Stoyko <sergeysinoptik@gmail.com>
2022-09-07 16:27:43 +0300

    swipe

--- app/components/bem-blocks/single-product-mob-content/single-product-mob-content.js
+++ app/components/bem-blocks/single-product-mob-content/single-product-mob-content.js
@@ -9,10 +9,12 @@ $(function() {
 	 
   //Создаем переменные
   var $productMobContent = $('.single-product-mob-content');
+  var swipeIcon = $('.swipe-icon');
+  var productName = $('.single-product-mob-name');
   var $body = $('body');
  
   //Закрытие меню свайпом
-  $('.single-product-mob-content').swipe({
+  $('.swipe-icon').swipe({
     swipeStatus: function(event, phase, direction, distance , duration , fingerCount) {
       console.log(direction)
       //Премещаем меню на проведенное расстояние
@@ -29,7 +31,7 @@ $(function() {
  
           //Смещаем меню на 0
           setTimeout(function() {
-            $menu.css('transform', 'translateX(0)');
+            $productMobContent.css('transform', 'translateX(0)');
           }, 500);
         }
         if (direction == 'down' && distance > 50) {
@@ -39,7 +41,41 @@ $(function() {
  
           //Смещаем меню на 0
           setTimeout(function() {
-            $menu.css('transform', 'translateX(0)');
+            $productMobContent.css('transform', 'translateX(0)');
+          }, 500);
+        }
+      }
+    },
+    threshold: 0
+  });
+  $('..single-product-mob-name').swipe({
+    swipeStatus: function(event, phase, direction, distance , duration , fingerCount) {
+      console.log(direction)
+      //Премещаем меню на проведенное расстояние
+      //$productMobContent.removeClass('single-product-mob-content_active');
+ 
+      //Если отпустили палец, то проводим следующее:
+      if (phase === $.fn.swipe.phases.PHASE_END) {
+ 
+        //Если сдвигаем меню влево и сдвинули более 100px
+        if (direction == 'up' && distance > 50) {
+ 
+          //Убираем классы у body
+          $productMobContent.addClass('single-product-mob-content_active');
+ 
+          //Смещаем меню на 0
+          setTimeout(function() {
+            $productMobContent.css('transform', 'translateX(0)');
+          }, 500);
+        }
+        if (direction == 'down' && distance > 50) {
+ 
+          //Убираем классы у body
+          $productMobContent.removeClass('single-product-mob-content_active');
+ 
+          //Смещаем меню на 0
+          setTimeout(function() {
+            $productMobContent.css('transform', 'translateX(0)');
           }, 500);
         }
       }
