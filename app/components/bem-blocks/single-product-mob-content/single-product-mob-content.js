eae58b390c71789af7fcbc75e66451b7bbb18054
Sergey Stoyko <sergeysinoptik@gmail.com>
2022-09-07 16:41:56 +0300

    swipe

--- app/components/bem-blocks/single-product-mob-content/single-product-mob-content.js
+++ app/components/bem-blocks/single-product-mob-content/single-product-mob-content.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(() => {
   jQuery('.swipe-icon').click(() => {
-    jQuery('.single-product-mob-content').toggleClass('single-product-mob-content_active').delay(1100).fadeIn('slow');
+    jQuery('.single-product-mob-content').toggleClass('single-product-mob-content_active'); // .delay(1100).fadeIn('slow');
   });
 });
 
