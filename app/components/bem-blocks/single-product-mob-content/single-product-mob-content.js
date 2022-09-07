20b4f377d55059c524638b6dc934d706b4b50af8
Sergey Stoyko <sergeysinoptik@gmail.com>
2022-09-07 15:52:57 +0300

    swipe

--- app/components/bem-blocks/single-product-mob-content/single-product-mob-content.js
+++ app/components/bem-blocks/single-product-mob-content/single-product-mob-content.js
@@ -14,7 +14,7 @@ $(function() {
   //Закрытие меню свайпом
   $('.single-product-mob-content').swipe({
     swipeStatus: function(event, phase, direction, distance , duration , fingerCount) {
- 
+      console.log(direction)
       //Премещаем меню на проведенное расстояние
       //$productMobContent.removeClass('single-product-mob-content_active');
  
@@ -22,10 +22,20 @@ $(function() {
       if (phase === $.fn.swipe.phases.PHASE_END) {
  
         //Если сдвигаем меню влево и сдвинули более 100px
-        if (direction == 'top' && distance > 100) {
+        if (direction == 'up' && distance > 100) {
  
           //Убираем классы у body
           $productMobContent.addClass('single-product-mob-content_active');
+ 
+          //Смещаем меню на 0
+          setTimeout(function() {
+            $menu.css('transform', 'translateX(0)');
+          }, 500);
+        }
+        if (direction == 'down' && distance > 100) {
+ 
+          //Убираем классы у body
+          $productMobContent.removeClass('single-product-mob-content_active');
  
           //Смещаем меню на 0
           setTimeout(function() {
