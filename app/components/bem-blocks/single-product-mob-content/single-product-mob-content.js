99835c6a18071acabb4c8f4d0b9d0d081459a45d
Sergey Stoyko <sergeysinoptik@gmail.com>
2022-09-07 09:27:14 +0300

    product page mobile description basic UI

--- app/components/bem-blocks/single-product-mob-content/single-product-mob-content.js
+++ app/components/bem-blocks/single-product-mob-content/single-product-mob-content.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(() => {
-  jQuery('.single-product-mob-description').click(() => {
+  jQuery('.swipe-icon').click(() => {
     jQuery('.single-product-mob-content').toggleClass('single-product-mob-content_active').delay(1100).fadeIn('slow');
   });
 });
