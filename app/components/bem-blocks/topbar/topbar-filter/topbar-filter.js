4bd6c393c1a71de9e66497db66368ef0cdc7e3cb
Sergey Stoyko <sergeysinoptik@gmail.com>
2023-08-17 23:29:37 +0300

    some fixes desktop

--- app/components/bem-blocks/topbar/topbar-filter/topbar-filter.js
+++ app/components/bem-blocks/topbar/topbar-filter/topbar-filter.js
@@ -1,16 +1,18 @@
-/* global window, document */
+/* global window, document, select */
 window.addEventListener('scroll', () => {
-  const body = document.querySelector('body');
   const filters = document.querySelector('.topbar-filter');
+  const products = select('.products');
   if (filters) {
-    if (window.pageYOffset > 85) {
+    if (window.pageYOffset > 306) {
       filters.classList.add('topbar-filter_sticky');
       filters.classList.remove('bg_grey');
       filters.classList.add('bg_white');
+      products.style.paddingTop = '110px';
     } else {
       filters.classList.remove('topbar-filter_sticky');
       filters.classList.remove('bg_white');
       filters.classList.add('bg_grey');
+      products.style.paddingTop = '0';
     }
   }
 }, true);
